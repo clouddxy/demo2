569db29e3ec0bdf3eb79c9b9efbc1591245f1388
daixiongyi <daixiongyi@bytedance.com>
2023-10-08 15:23:58 +0800

    feat: add
    Change-Id: I3d749b0048d8366c593bef9addcfda78c9ca6246

---
+++ helper.go
@@ -0,0 +1,17 @@
+package helper
+
+import (
+	"github.com/clouddxy/demo5/helper"
+)
+
+func Add(a, b int) int {
+	return helper.AddDemo5(a, b)
+}
+
+func AddInt64(a, b int64) int64 {
+	return a + b
+}
+
+func AddUInt64(a, b uint64) uint64 {
+	return a + b
+}
